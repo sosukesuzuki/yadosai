b6b7b8ea0009b7168be547490b2ba65b3c5d3b31
Sosuke Suzuki <aosukeke@gmail.com>
2019-04-24 22:33:59 +0900

    仮UIを実装

--- src/components/pages/Sale.tsx
+++ src/components/pages/Sale.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 const Sale: React.FC = () => {
   return (
     <>
       <Typography variant="h5">メロンパンアイスを売るぞ!</Typography>
+      <Typography variant="h5">残量: 30個</Typography>
+      <Button fullWidth variant="contained" size="large" color="primary">
+        売った
+      </Button>
     </>
   );
 };

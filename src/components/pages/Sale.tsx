5bdf85881479a06b57c793fddfbed24dde8aebe8
Sosuke Suzuki <aosukeke@gmail.com>
2019-04-25 00:14:47 +0900

    リアルタイム取得

--- src/components/pages/Sale.tsx
+++ src/components/pages/Sale.tsx
@@ -1,17 +1,19 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import useMelonpanIceStock from "../../lib/hooks/useMelonpanIceStock";
 import sellMelonpanIce from "../../lib/sellMelonpanIce";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const Sale: React.FC = () => {
   const stock = useMelonpanIceStock();
-  const isLoading = useMemo(() => stock === null, [stock]);
+  const [isLoading, setIsLoading] = useState(false);
+  const isStockNull = useMemo(() => stock === null, [stock]);
 
   return (
     <>
       <Typography variant="h5">メロンパンアイスを売るぞ!</Typography>
-      {!isLoading && (
+      {!isLoading && !isStockNull ? (
         <>
           <Typography variant="h5">残量: {stock}個</Typography>
           <Button
@@ -20,12 +22,19 @@ const Sale: React.FC = () => {
             size="large"
             color="primary"
             onClick={() => {
-              sellMelonpanIce();
+              setIsLoading(true);
+              sellMelonpanIce().then(() => {
+                setIsLoading(false);
+              });
             }}
           >
             売った
           </Button>
         </>
+      ) : (
+        <div>
+          <CircularProgress color="primary" />
+        </div>
       )}
     </>
   );
--- src/lib/hooks/useMelonpanIceStock.ts
+++ src/lib/hooks/useMelonpanIceStock.ts
@@ -4,11 +4,10 @@ import { melonpanice } from "../db";
 export default function useMelonpanIceStock() {
   const [stock, setStock] = useState<number | null>(null);
   useEffect(() => {
-    melonpanice.get().then(documentSnapshot => {
-      const melonpanice = documentSnapshot.data();
-      if (melonpanice == null) throw new Error("データベース接続エラー");
-      setStock(melonpanice.stock);
+    melonpanice.onSnapshot(doc => {
+      const { stock } = doc.data()!;
+      setStock(stock);
     });
-  });
+  }, []);
   return stock;
 }

5de314a121c4cd2b7d0b7f95430080985a35cd32
Sosuke Suzuki <aosukeke@gmail.com>
2019-04-24 13:54:41 +0900

    MaterialUI 導入:cry:

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import withStyles, {
+  StyleRules,
+  WithStyles
+} from "@material-ui/core/styles/withStyles";
+import { createStyles } from "@material-ui/core";
+
+const styles: StyleRules = createStyles({
+  root: {
+    padding: "10px 30px"
+  }
+});
+
+type Props = WithStyles<typeof styles>;
+
+const Header: React.FC<Props> = ({ classes }) => (
+  <AppBar className={classes.root}>
+    <Typography variant="h6" color="inherit">
+      メロンパンアイスを売りたい!!
+    </Typography>
+  </AppBar>
+);
+
+export default withStyles(styles)(Header);

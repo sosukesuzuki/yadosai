1a4fa26e096c3d9d5b9ae02ed665f82ad97ae192
Sosuke Suzuki <aosukeke@gmail.com>
2019-04-25 13:12:59 +0900

    トランザクションに失敗したときには履歴を更新しない

--- src/lib/sellMelonpanIce.ts
+++ src/lib/sellMelonpanIce.ts
@@ -3,15 +3,19 @@ import db, { melonpanice, salesHisory } from "./db";
 import { getName } from "./localStorage";
 
 export default async function sellMelonpanIce(): Promise<void> {
-  await db.runTransaction(async transaction => {
-    const melonpaniceDoc = await transaction.get(melonpanice);
-    if (!melonpaniceDoc.exists) {
-      throw "Document does not exist!";
-    }
+  try {
+    await db.runTransaction(async transaction => {
+      const melonpaniceDoc = await transaction.get(melonpanice);
+      if (!melonpaniceDoc.exists) {
+        throw "Document does not exist!";
+      }
 
-    const newStock = melonpaniceDoc.data()!.stock - 1;
-    transaction.update(melonpanice, { stock: newStock });
-  });
+      const newStock = melonpaniceDoc.data()!.stock - 1;
+      transaction.update(melonpanice, { stock: newStock });
+    });
+  } catch (error) {
+    throw error;
+  }
 
   await salesHisory.add({
     name: getName(),

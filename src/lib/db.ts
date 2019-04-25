009ceb0e01ac3eb4ac3213981b0f55919d377ef1
Sosuke Suzuki <aosukeke@gmail.com>
2019-04-25 13:05:48 +0900

    売上時に履歴を追加

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -12,5 +12,6 @@ firebase.initializeApp({
 const db = firebase.firestore();
 
 export const melonpanice = db.collection("food").doc("melonpanice");
+export const salesHisory = db.collection("melonpanice_sales_history");
 
 export default db;
--- src/lib/sellMelonpanIce.ts
+++ src/lib/sellMelonpanIce.ts
@@ -1,14 +1,20 @@
-import db, { melonpanice } from "./db";
+import format from "date-fns/format";
+import db, { melonpanice, salesHisory } from "./db";
+import { getName } from "./localStorage";
 
-export default function sellMelonpanIce(): Promise<void> {
-  return db.runTransaction(transaction => {
-    return transaction.get(melonpanice).then(melonpaniceDoc => {
-      if (!melonpaniceDoc.exists) {
-        throw "Document does not exist!";
-      }
+export default async function sellMelonpanIce(): Promise<void> {
+  await db.runTransaction(async transaction => {
+    const melonpaniceDoc = await transaction.get(melonpanice);
+    if (!melonpaniceDoc.exists) {
+      throw "Document does not exist!";
+    }
 
-      const newStock = melonpaniceDoc.data()!.stock - 1;
-      transaction.update(melonpanice, { stock: newStock });
-    });
+    const newStock = melonpaniceDoc.data()!.stock - 1;
+    transaction.update(melonpanice, { stock: newStock });
+  });
+
+  await salesHisory.add({
+    name: getName(),
+    createdAt: format(new Date(), "YYYY/MM/DD HH:MM")
   });
 }

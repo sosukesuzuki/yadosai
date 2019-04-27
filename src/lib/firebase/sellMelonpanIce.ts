f0953fdf80c41621abd8e8862889549ed66f2420
Sosuke Suzuki <aosukeke@gmail.com>
2019-04-27 14:06:25 +0900

    購入履歴と一緒にユーザーのuidを含める

--- src/lib/firebase/sellMelonpanIce.ts
+++ src/lib/firebase/sellMelonpanIce.ts
@@ -1,4 +1,5 @@
 import format from "date-fns/format";
+import firebase from "./firebase";
 import db, { melonpanice, salesHisory } from "./db";
 
 export default async function sellMelonpanIce(): Promise<void> {
@@ -16,8 +17,12 @@ export default async function sellMelonpanIce(): Promise<void> {
     throw error;
   }
 
-  // TODO: uidを一緒におくる
+  const user = firebase.auth().currentUser;
+
+  if (user === null) throw new Error("ユーザーがnull");
+
   await salesHisory.add({
+    userId: user.uid,
     createdAt: format(new Date(), "YYYY/MM/DD HH:MM")
   });
 }
